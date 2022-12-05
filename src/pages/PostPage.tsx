f831661993c9006c4783df05c0b4fc5d8a7590c6
ocha <okamonster0615@gmail.com>
2022-12-06 04:56:07 +0900

    Add dependance:three関係のライブラリを追加

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,8 +1,21 @@
 import styled from "@emotion/styled";
-import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
+import { ArrowBack, ArrowBackIos, ArrowDownward, ArrowForward, ArrowForwardIos, ArrowUpward, List } from "@mui/icons-material";
 import { Button } from "@mui/material";
+import { Joystick } from 'react-joystick-component';
 
 export const PostPage = () => {
+    const handleMove = (e: any) => {
+        console.log(e);
+    }
+
+    const handleStart = (e: any) => {
+        console.log(e);
+    }
+
+    const handleStop = (e: any) => {
+        console.log(e);
+    }
+
     return(
         <>
             <PostHeader>
@@ -13,6 +26,23 @@ export const PostPage = () => {
                     <ArrowForwardIos/>
                 </Button>
             </PostHeader>
+            
+            <Controller>
+                <Joystick size={80} move={handleMove} start={handleStart} stop={handleStop}
+                baseColor={"#808080"} stickColor={"#000"}/>
+                
+            </Controller>
+            <SlotBar>
+                    <OptionSlot>
+                        <List sx={{color:"#fff",fontSize:30}}/>
+                    </OptionSlot>
+                    <Slot/>
+                    <Slot/>
+                    <Slot/>
+                    <Slot/>
+                </SlotBar>
+            
+            
         </>
     );
 }
@@ -22,7 +52,39 @@ const PostHeader = styled.header`
     height:60px;
     justify-content:space-between;
     padding:0 15px;
+    width:100%;
 `;
 
+const Controller = styled.div`
+    padding:0 15px;
+    text-align:center;
+    width:100%;
+`;
 
+const SlotBar = styled.div`
+    display:inline-flex;
+    background-color:#000;
+    border-radius:3px;
 
+`;
+
+const Slot = styled.div`
+    height:40px;
+    width:40px;
+    boeder-radius:5px;
+    border:solid 2px #e0e8e8;
+    background-color:#808080;
+    margin:5px 3px;
+`
+
+const OptionSlot = styled.div`
+    height:40px;
+    width:40px;
+    boeder-radius:5px;
+    border:solid 2px #e0e8e8;
+    background-color:#000;
+    margin:5px 3px;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+`
